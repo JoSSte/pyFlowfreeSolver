47559a4de2118fc53ef20f52d9e99501f4bd8cd0
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-11-20 19:52:06 +0100

    column and row count implemented

--- shapeDetection.py
+++ shapeDetection.py
@@ -1,9 +1,8 @@
 from win32api import GetSystemMetrics
 import numpy as np
 import cv2
-from scipy.interpolate import splprep, splev
 
-# the maximum width/height of a table
+# the maximum width/height of a table in cell count
 MAX_TABLE_DIMENSION = 15
 
 #resize image to fit on the screen
@@ -30,27 +29,36 @@ def detectCircles(image, displayImage = False):
     output = image.copy()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # detect circles in the image
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1=50, param2=40, minRadius=55, maxRadius=120)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1=50, param2=40, minRadius=45, maxRadius=120)
     # ensure at least some circles were found
     if circles is not None:
         # convert the (x, y) coordinates and radius of the circles to integers
         circles = np.round(circles[0, :]).astype("int")
-        # loop over the (x, y) coordinates and radius of the circles
-        for idx, (x, y, r) in enumerate(circles):
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(output, (x - 5, y - 5),(x + 5, y + 5), (0, 128, 255), -1)
-            cv2.putText(output, str(idx), (x - 7, y - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (60,60,60), 2, cv2.LINE_AA )
-            #print("x: %d\ty: %d\tr: %d" % (x, y, r))
-        # show the output image
         if(displayImage):
+            # loop over the (x, y) coordinates and radius of the circles
+            for idx, (x, y, r) in enumerate(circles):
+                # draw the circle in the output image, then draw a rectangle
+                # corresponding to the center of the circle
+                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(output, (x - 5, y - 5),(x + 5, y + 5), (0, 128, 255), -1)
+                cv2.putText(output, str(idx), (x - 7, y - 7), cv2.FONT_HERSHEY_SIMPLEX, 1, (60,60,60), 2, cv2.LINE_AA )
+                #print("x: %d\ty: %d\tr: %d" % (x, y, r))
+        # show the output image
             cv2.imshow("output", get_resized_for_display_img(np.hstack([image, output])))
             cv2.waitKey(0)
+        
+        if(circles is None):
+            circles = []
         return circles
 
-def detectGrid(image):
-    print("detecting grid")
+def isSquare(n: int):
+    return ( np.sqrt(n) %1) == 0
+
+def detectGrid(image, displayImage = False):
+    #the array of squares
+    squares = []
+
+    #print("detecting grid")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (9,9), 0)
     thresh1 = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
@@ -58,12 +66,12 @@ def detectGrid(image):
     contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     max_area = 0
     c = 0
-    for i in contours1:
-        area = cv2.contourArea(i)
+    for ctr1 in contours1:
+        area = cv2.contourArea(ctr1)
         if area > 1000:
                 if area > max_area:
                     max_area = area
-                    best_cnt = i
+                    best_cnt = ctr1
                     image = cv2.drawContours(image, contours1, c, (0, 255, 0), 3)
         c+=1
     #best_cnt = smoothContour(best_cnt)
@@ -91,26 +99,51 @@ def detectGrid(image):
 
     #calculate minimum area of a cell
     minarea = np.square(image.shape[0]/MAX_TABLE_DIMENSION)
-    a = 0
-    for c, i in enumerate(contours):
-        area = cv2.contourArea(i)
+    numSquares = 0
+
+    #coordinates of the first encountered piece to count rows and columns
+    firstX = 0
+    firstY = 0
+    #counters to count rows and columns
+    numX = 0
+    numY = 0
+    for c, ctr in enumerate(contours):
+        area = cv2.contourArea(ctr)
         if area > max_area:
             gameboard = c
-            print("found board @ %i" % (hierarchy[gameboard, c, 3]))
+            #print("found board @ %ctr" % (hierarchy[gameboard, c, 3]))
         if hierarchy[gameboard, c,3] == 1:
-            a = a+1
-            if area > minarea/2:
-                p = int(np.abs(155 - np.abs(hierarchy[gameboard, c,3])))
-                cv2.drawContours(image, contours, c, (p, 255, p), 3)
+            M = cv2.moments(ctr)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # Save the first square's coordinates
+            if (firstX == 0 and firstY == 0):
+                firstX = cX
+                firstY = cY
+                numX = numX + 1
+                numY = numY + 1
+            else:
+            # Check if we are in the same row or column as the first square (center within 5px)
+                if np.abs(cX-firstX) < 5:
+                    numX = numX + 1
+                if np.abs(cY-firstY) < 5:
+                    numY = numY + 1
+            squares.append((cX,cY))
+            numSquares = numSquares+1
+            if displayImage and area > minarea/2:
+                cv2.drawContours(image, contours, c, (0, 255, 0), 3)
     
-    print(a)
+    #print("Dimensions: x: %d y:%d" % (numX, numY))
 
-    cv2.imshow("thresholds", get_resized_for_display_img(thresh))
-    #cv2.imshow("mask", mask)
-    cv2.imshow("New image", get_resized_for_display_img(out))
-    cv2.imshow("Anded Color", get_resized_for_display_img(bit_and))
-    #cv2.imshow("blur1", blur)
-    #cv2.imshow("thresh1", thresh)
-    cv2.imshow("Final Image", get_resized_for_display_img(image))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if displayImage:    
+        cv2.imshow("thresholds", get_resized_for_display_img(thresh))
+        #cv2.imshow("mask", mask)
+        cv2.imshow("New image", get_resized_for_display_img(out))
+        cv2.imshow("Anded Color", get_resized_for_display_img(bit_and))
+        #cv2.imshow("blur1", blur)
+        #cv2.imshow("thresh1", thresh)
+        cv2.imshow("Final Image", get_resized_for_display_img(image))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    return squares, circles, int(numX), int(numY)
